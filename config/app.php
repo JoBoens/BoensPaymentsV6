1c9e7cb0795aeac40fac79893743b1523e752f53
JoBoens <jo@boens.org>
2026-07-11 17:27:52 +0200

    V6.0.1 - Configuration files

---
+++ config/app.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+return [
+
+    'name'        => 'Boens Payments',
+    'version'     => '6.0.1',
+    'environment' => 'development',
+
+    'timezone'    => 'Europe/Brussels',
+    'locale'      => 'nl_BE',
+
+    'currency'    => 'EUR',
+
+    'date_format' => 'd/m/Y',
+
+];
---
+++ config/database.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+return [
+
+    'host'     => '',
+    'database' => '',
+    'username' => '',
+    'password' => '',
+    'charset'  => 'utf8mb4',
+
+];
