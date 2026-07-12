fa0b51f27404e11edfb40004d30e3b4256c2189a
JoBoens <jo@boens.org>
2026-07-12 10:12:17 +0200

    V6.2.0 - Default categories

---
+++ database/database/V6_002_default_categories.sql
@@ -0,0 +1,44 @@
+/*
+|--------------------------------------------------------------------------
+| Boens Payments V6
+|--------------------------------------------------------------------------
+| Bestand : V6_002_default_categories.sql
+| Versie  : 6.1.0
+| Doel    : Standaard categorieën
+|--------------------------------------------------------------------------
+*/
+
+INSERT INTO categorieen (naam, kleur, icoon) VALUES
+
+('Algemeen',       '#173A63', 'folder'),
+
+('Elektriciteit',  '#F4B400', 'bolt'),
+('Gas',            '#F57C00', 'fire'),
+('Water',          '#039BE5', 'droplet'),
+
+('Internet',       '#1565C0', 'wifi'),
+('Telefoon',       '#3949AB', 'phone'),
+
+('Verzekering',    '#00897B', 'shield'),
+('Belastingen',    '#6D4C41', 'landmark'),
+
+('Voeding',        '#43A047', 'basket-shopping'),
+('Restaurant',     '#8E24AA', 'utensils'),
+
+('Gezondheid',     '#D81B60', 'heart-pulse'),
+('Apotheek',       '#C2185B', 'capsules'),
+
+('Auto',           '#546E7A', 'car'),
+('Brandstof',      '#EF6C00', 'gas-pump'),
+('Openbaar vervoer','#00838F','train'),
+
+('Sport',          '#2E7D32', 'dumbbell'),
+('Kleding',        '#7B1FA2', 'shirt'),
+
+('Vakantie',       '#1E88E5', 'umbrella-beach'),
+
+('Abonnementen',   '#5E35B1', 'repeat'),
+
+('Huishouden',     '#6D4C41', 'house'),
+
+('Divers',         '#757575', 'boxes');
